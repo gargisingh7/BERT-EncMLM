16bae8f49352996e3fe7522f4ffce9bf35aa68cd
Gargi Singh <40858250+gargisingh7@users.noreply.github.com>
2021-05-20 14:44:08 +0530

    Update model_distil.py

--- model_distil.py
+++ model_distil.py
@@ -9,37 +9,57 @@ class DistilBertForEncoderMLM(DistilBertPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
 
-        self.bert = DistilBertModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.cls_lm = BertOnlyMLMHead(config)
-        self.loss_fct = CrossEntropyLoss()
+        self.distilbert = DistilBertModel(config)
+        self.vocab_transform = nn.Linear(config.dim, config.dim)
+        self.vocab_layer_norm = nn.LayerNorm(config.dim, eps=1e-12)
+        self.vocab_projector = nn.Linear(config.dim, config.vocab_size)
+
         self.init_weights()
+
+        self.mlm_loss_fct = nn.CrossEntropyLoss()
+
+        
+#         self.dropout = nn.Dropout(config.hidden_dropout_prob)
+#         self.cls_lm = BertOnlyMLMHead(config)
+#         self.loss_fct = CrossEntropyLoss()
+#         self.init_weights()
 
     def forward(
             self,
             input_ids=None,
             attention_mask=None,
-            token_type_ids=None,
-            position_ids=None,
             head_mask=None,
             inputs_embeds=None,
             labels=None,
-            masked_lm_labels=None,
+            output_attentions=None,
+            output_hidden_states=None,
+            return_dict=None,
     ):
-        outputs = self.bert(
-            input_ids,
+        dlbrt_output = self.distilbert(
+            input_ids=input_ids,
             attention_mask=attention_mask,
-            token_type_ids=token_type_ids,
-            position_ids=position_ids,
             head_mask=head_mask,
             inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
         )
+        hidden_states = dlbrt_output[0]  # (bs, seq_length, dim)
+        prediction_logits = self.vocab_transform(hidden_states)  # (bs, seq_length, dim)
+        prediction_logits = gelu(prediction_logits)  # (bs, seq_length, dim)
+        prediction_logits = self.vocab_layer_norm(prediction_logits)  # (bs, seq_length, dim)
+        prediction_logits = self.vocab_projector(prediction_logits)  # (bs, seq_length, vocab_size)
 
-        sequence_output = outputs[0]
-        prediction_scores = self.cls_lm(sequence_output)
-        loss = self.loss_fct(prediction_scores.view(-1, self.config.vocab_size), masked_lm_labels.view(-1)) # -100 index = padding token; using ignore_index feature
+        mlm_loss = None
+        if labels is not None:
+            mlm_loss = self.mlm_loss_fct(prediction_logits.view(-1, prediction_logits.size(-1)), labels.view(-1))
+        outputs = (mlm_loss,) + outputs
 
-        outputs = (loss,) + outputs
+#         sequence_output = outputs[0]
+#         prediction_scores = self.cls_lm(sequence_output)
+#         loss = self.loss_fct(prediction_scores.view(-1, self.config.vocab_size), masked_lm_labels.view(-1)) # -100 index = padding token; using ignore_index feature
+
+#         outputs = (loss,) + outputs
 
         return outputs  # (loss), logits, (hidden_states), (attentions)
 
