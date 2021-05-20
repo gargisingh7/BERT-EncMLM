304de4fd691532a2428c6a831e336a0ab07dc6f0
Gargi Singh <40858250+gargisingh7@users.noreply.github.com>
2021-05-20 20:29:22 +0530

    Update model_distil.py

--- model_distil.py
+++ model_distil.py
@@ -18,23 +18,38 @@ class DistilBertForEncoderMLM(DistilBertPreTrainedModel):
 
         self.mlm_loss_fct = nn.CrossEntropyLoss()
 
-        
-#         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-#         self.cls_lm = BertOnlyMLMHead(config)
-#         self.loss_fct = CrossEntropyLoss()
-#         self.init_weights()
+    def get_output_embeddings(self):
+        return self.vocab_projector
 
+    def set_output_embeddings(self, new_embeddings):
+        self.vocab_projector = new_embeddings
+
+[DOCS]    @add_start_docstrings_to_model_forward(DISTILBERT_INPUTS_DOCSTRING.format("batch_size, num_choices"))
+    @add_code_sample_docstrings(
+        tokenizer_class=_TOKENIZER_FOR_DOC,
+        checkpoint=_CHECKPOINT_FOR_DOC,
+        output_type=MaskedLMOutput,
+        config_class=_CONFIG_FOR_DOC,
+    )
     def forward(
-            self,
-            input_ids=None,
-            attention_mask=None,
-            head_mask=None,
-            inputs_embeds=None,
-            labels=None,
-            output_attentions=None,
-            output_hidden_states=None,
-            return_dict=None,
+        self,
+        input_ids=None,
+        attention_mask=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
     ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`):
+            Labels for computing the masked language modeling loss. Indices should be in ``[-100, 0, ...,
+            config.vocab_size]`` (see ``input_ids`` docstring) Tokens with indices set to ``-100`` are ignored
+            (masked), the loss is only computed for the tokens with labels in ``[0, ..., config.vocab_size]``.
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
         dlbrt_output = self.distilbert(
             input_ids=input_ids,
             attention_mask=attention_mask,
@@ -53,6 +68,7 @@ class DistilBertForEncoderMLM(DistilBertPreTrainedModel):
         mlm_loss = None
         if labels is not None:
             mlm_loss = self.mlm_loss_fct(prediction_logits.view(-1, prediction_logits.size(-1)), labels.view(-1))
+
         outputs = (mlm_loss,) + outputs
 
 #         sequence_output = outputs[0]
